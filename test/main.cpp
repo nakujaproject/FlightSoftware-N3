a62acc9bd71f30ce121e8c1018dd5bf0bdfe85bf
bytecod31 <emwiti658@gmail.com>
2023-03-29 12:43:54 +0300

    added FSM code

---
+++ test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "test_class.h"
+
+int main(){
+    Test t;
+    t.y = 89;
+
+    std::cout<< t.y <<std::endl;
+
+    return 0;
+}
---
+++ test/test_class.cpp
@@ -0,0 +1,7 @@
+#include "test_class.h"
+
+int Test::foo(){
+    int x = 9;
+    return x;
+}
+
